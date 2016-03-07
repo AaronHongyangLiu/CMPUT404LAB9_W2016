939c3087a2042c04e0a3e6abcfbf442ebb105219
Alexander Wong <admin@alexander-wong.com>
2016-03-06 23:05:08 -0700

    Added iguana client app

--- iguana/iguana/urls.py
+++ iguana/iguana/urls.py
@@ -16,16 +16,18 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 from rest_framework import routers
-from quickstart import views
+from quickstart import views as quickstart_views
+from client import views as client_views
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+router.register(r'users', quickstart_views.UserViewSet)
+router.register(r'groups', quickstart_views.GroupViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include(router.urls)),
-    url(r'^api/auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api/auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^', client_views.example_view)
 ]
